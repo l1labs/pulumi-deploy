dba95fb280b306627c9114b7bb0e2b1b201752c3
Mike Wiles <mike@wilesfarm.com>
2022-09-17 13:01:31 -0600

    Remove deprecated libraries and added logging
    - Removed calls into deprecated methods 
    - Updated to latest pulumi structures 
    - Added a logging configuration option ECS.EnableLogging

--- aws/ecs.go
+++ aws/ecs.go
@@ -3,13 +3,16 @@ package aws
 import (
 	"fmt"
 
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/cloudwatch"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecs"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/kms"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ECS struct {
-	Name string
+	Name          string
+	EnableLogging bool
 
 	Out struct {
 		Cluster      *ecs.Cluster
@@ -29,34 +32,69 @@ func (e *ECS) Run(ctx *pulumi.Context) error {
 	if err := e.Validate(); err != nil {
 		return err
 	}
+	var cluster *ecs.Cluster
+	if e.EnableLogging {
+		logKey, err := kms.NewKey(ctx, fmt.Sprintf("%v-log-key", e.Name), &kms.KeyArgs{
+			Description:          pulumi.String(fmt.Sprintf("%v KMS encryption key for logging container activity", e.Name)),
+			DeletionWindowInDays: pulumi.Int(7),
+		})
+		if err != nil {
+			return err
+		}
+		ctx.Export("CLUSTER-LOG-KMS-KEY-ID", logKey.ID())
 
-	// Create ECS cluster
-	cluster, err := ecs.NewCluster(ctx, e.Name, &ecs.ClusterArgs{
-		CapacityProviders: pulumi.StringArray{},
-		Name:              pulumi.String(e.Name),
-		Settings:          nil,
-		Tags:              nil,
-	})
-	if err != nil {
-		return err
+		logGroup, err := cloudwatch.NewLogGroup(ctx, fmt.Sprintf("%v-log-group", e.Name), nil)
+		if err != nil {
+			return err
+		}
+		ctx.Export("CLUSTER-LOG-GROUP-ID", logGroup.ID())
+
+		cluster, err = ecs.NewCluster(ctx, e.Name, &ecs.ClusterArgs{
+			Configuration: &ecs.ClusterConfigurationArgs{
+				ExecuteCommandConfiguration: &ecs.ClusterConfigurationExecuteCommandConfigurationArgs{
+					KmsKeyId: logKey.Arn,
+					Logging:  pulumi.String("OVERRIDE"),
+					LogConfiguration: &ecs.ClusterConfigurationExecuteCommandConfigurationLogConfigurationArgs{
+						CloudWatchEncryptionEnabled: pulumi.Bool(true),
+						CloudWatchLogGroupName:      logGroup.Name,
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+	} else {
+		var err error
+		cluster, err = ecs.NewCluster(ctx, e.Name, &ecs.ClusterArgs{
+			Settings: ecs.ClusterSettingArray{
+				&ecs.ClusterSettingArgs{
+					Name:  pulumi.String("containerInsights"),
+					Value: pulumi.String("enabled"),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
 	}
-
 	e.Out.Cluster = cluster
 	ctx.Export("CLUSTER-ID", cluster.ID())
 
 	// Create IAM role that can be used by our service's task.
 	taskExecRole, err := iam.NewRole(ctx, fmt.Sprintf("%v-task-exec-role", e.Name), &iam.RoleArgs{
-		AssumeRolePolicy: pulumi.String(`{
-	"Version": "2008-10-17",
-	"Statement": [{
-		"Sid": "",
-		"Effect": "Allow",
-		"Principal": {
-			"Service": "ecs-tasks.amazonaws.com"
-		},
-		"Action": "sts:AssumeRole"
-	}]
-}`),
+		AssumeRolePolicy: pulumi.String(
+			`{
+				"Version": "2008-10-17",
+				"Statement": [{
+					"Sid": "",
+					"Effect": "Allow",
+					"Principal": {
+						"Service": "ecs-tasks.amazonaws.com"
+					},
+					"Action": "sts:AssumeRole"
+				}]
+			}`),
 	})
 	if err != nil {
 		return err
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/l1labs/pulumi-deploy
 go 1.15
 
 require (
-	github.com/pulumi/pulumi-aws/sdk/v5 v5.13.0
+	github.com/pulumi/pulumi-aws/sdk/v5 v5.14.0
 	github.com/pulumi/pulumi-docker/sdk/v3 v3.4.1
 	github.com/pulumi/pulumi/sdk/v3 v3.39.3
 	github.com/stretchr/testify v1.7.0
