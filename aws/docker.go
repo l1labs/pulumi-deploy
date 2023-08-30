060220eb462c97d16304b7c4547184d7436ff26f
Scott Raine <scott@enspire.com>
2023-08-30 15:54:46 -0500

    Extract docker image creation

---
+++ aws/docker.go
@@ -0,0 +1,90 @@
+package aws
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecr"
+	"github.com/pulumi/pulumi-docker/sdk/v3/go/docker"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type Docker struct {
+	Name   string
+	Docker *docker.DockerBuildArgs
+
+	Out struct {
+		Repo  *ecr.Repository
+		Image *docker.Image
+	}
+}
+
+func (d *Docker) Validate() error {
+	if d.Name == "" {
+		return fmt.Errorf("Missing Name")
+	}
+
+	if d.Docker == nil {
+		return fmt.Errorf("Missing docker.Docker args")
+	}
+
+	return nil
+}
+
+func (d *Docker) Run(ctx *pulumi.Context) error {
+	// Create docker repo
+	repo, err := ecr.NewRepository(ctx, d.Name, &ecr.RepositoryArgs{
+		Name: pulumi.String(d.Name),
+		EncryptionConfigurations: ecr.RepositoryEncryptionConfigurationArray{
+			&ecr.RepositoryEncryptionConfigurationArgs{
+				EncryptionType: pulumi.String("AES256"),
+			},
+		},
+		ImageTagMutability: pulumi.String("MUTABLE"),
+		ImageScanningConfiguration: &ecr.RepositoryImageScanningConfigurationArgs{
+			ScanOnPush: pulumi.Bool(true),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	d.Out.Repo = repo
+
+	// Get repository credentials
+	repoCreds := repo.RegistryId.ApplyT(func(rid string) ([]string, error) {
+		creds, err := ecr.GetCredentials(ctx, &ecr.GetCredentialsArgs{
+			RegistryId: rid,
+		})
+		if err != nil {
+			return nil, err
+		}
+		data, err := base64.StdEncoding.DecodeString(creds.AuthorizationToken)
+		if err != nil {
+			return nil, err
+		}
+		return strings.Split(string(data), ":"), nil
+	}).(pulumi.StringArrayOutput)
+
+	repoUser := repoCreds.Index(pulumi.Int(0))
+	repoPass := repoCreds.Index(pulumi.Int(1))
+
+	// Create image
+	image, err := docker.NewImage(ctx, d.Name, &docker.ImageArgs{
+		Build:     *d.Docker,
+		ImageName: repo.RepositoryUrl,
+		Registry: docker.ImageRegistryArgs{
+			Server:   repo.RepositoryUrl,
+			Username: repoUser,
+			Password: repoPass,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	d.Out.Image = image
+
+	return nil
+}
--- aws/service.go
+++ aws/service.go
@@ -1,14 +1,10 @@
 package aws
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/cloudwatch"
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecr"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ecs"
 	"github.com/pulumi/pulumi-docker/sdk/v3/go/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -73,51 +69,16 @@ func (s *Service) Run(ctx *pulumi.Context) error {
 		return err
 	}
 
-	// Create docker repo
-	repo, err := ecr.NewRepository(ctx, s.Name, &ecr.RepositoryArgs{
-		Name: pulumi.String(s.Name),
-		EncryptionConfigurations: ecr.RepositoryEncryptionConfigurationArray{
-			&ecr.RepositoryEncryptionConfigurationArgs{
-				EncryptionType: pulumi.String("AES256"),
-			},
-		},
-		ImageTagMutability: pulumi.String("MUTABLE"),
-		ImageScanningConfiguration: &ecr.RepositoryImageScanningConfigurationArgs{
-			ScanOnPush: pulumi.Bool(true),
-		},
-	})
-	if err != nil {
+	d := &Docker{
+		Name:   s.Name,
+		Docker: s.Docker,
+	}
+
+	if err := d.Validate(); err != nil {
 		return err
 	}
 
-	// Get repository credentials
-	registryInfo := repo.RegistryId.ApplyT(func(id string) (docker.ImageRegistry, error) {
-		creds, err := ecr.GetCredentials(ctx, &ecr.GetCredentialsArgs{RegistryId: id})
-		if err != nil {
-			return docker.ImageRegistry{}, err
-		}
-		decoded, err := base64.StdEncoding.DecodeString(creds.AuthorizationToken)
-		if err != nil {
-			return docker.ImageRegistry{}, err
-		}
-		parts := strings.Split(string(decoded), ":")
-		if len(parts) != 2 {
-			return docker.ImageRegistry{}, errors.New("invalid credentials")
-		}
-		return docker.ImageRegistry{
-			Server:   creds.ProxyEndpoint,
-			Username: parts[0],
-			Password: parts[1],
-		}, nil
-	}).(docker.ImageRegistryOutput)
-
-	// Create image
-	image, err := docker.NewImage(ctx, s.Name, &docker.ImageArgs{
-		Build:     *s.Docker,
-		ImageName: repo.RepositoryUrl,
-		Registry:  registryInfo,
-	})
-	if err != nil {
+	if err := d.Run(ctx); err != nil {
 		return err
 	}
 
@@ -132,7 +93,7 @@ func (s *Service) Run(ctx *pulumi.Context) error {
 	}
 
 	// Create container definition
-	containerDef := pulumi.All(image.ImageName, s.Env, s.DockerLabels, s.SidecarContainers, logConfiguration).ApplyT(
+	containerDef := pulumi.All(d.Out.Image.ImageName, s.Env, s.DockerLabels, s.SidecarContainers, logConfiguration).ApplyT(
 		func(args []interface{}) (string, error) {
 			image := args[0].(string)
 
